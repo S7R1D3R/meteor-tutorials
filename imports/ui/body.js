3edc60e10aba91b5621b5bf6e8b08dcecd2c19b8
Joaquim T. Duarte <joaquimtd.info@gmail.com>
2017-02-28 17:58:25 +0000

    Temporary UI State

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,17 +1,33 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
-    return Tasks.find({}, {sort: {createdAt: -1} });
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
 Template.body.events({
+
   'submit .new-task'(event) {
     // Prevent default browser form submit
     event.preventDefault();
@@ -23,10 +39,16 @@ Template.body.events({
     // Insert a task into the collection
     Tasks.insert({
       text,
-      createdAt: new Date(), // current time
+      createdAt: new Date(),
+      owner: Meteor.userID(),
+      username: Meteor.user().username,
     });
 
     // Clear form
     target.text.value = '';
+  },
+
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
